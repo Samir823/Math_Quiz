edf7860e69558fa9f1b4e7592fee51ccb14611d0
filip <filipdautovic@yahoo.com>
2016-11-16 12:49:06 +0100

    oduzimanje dodano

--- src/Main_app.java
+++ src/Main_app.java
@@ -4,17 +4,9 @@ public class Main_app {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
+		System.out.println(substraction());
 	}
 
-	public static boolean addition() {
-		double num1 = 1 + Math.random() * 10;
-		double num2 = 1 + Math.random() * 10;
-=======
-	
-		System.out.println(addition());
-		}
 
 	public static boolean addition() {
 		
@@ -34,11 +26,25 @@ public class Main_app {
 		System.out.println("Odgovor netacan.");
 		return false;	
 	
->>>>>>> 07fb2ef27ba4c591d40009654717a8ec5ceb84dd
 	}
 
-	public static void substraction() {
-
+	public static boolean substraction() {
+		
+		int num1 = 1 + (int)(Math.random() * 10);
+		int num2 = 1 + (int)(Math.random() * 10);
+		
+		int rez = num1 - num2;
+		
+		System.out.println("Unesite odgovor: " +num1+ " - " +num2);
+		int user = (int)scanner();
+		
+		if(rez == user){
+			System.out.println("Odgovor tacan.");
+			return true;
+			
+		}
+		System.out.println("Odgovor netacan.");
+		return false;	
 	}
 
 	public static void multiplication() {
