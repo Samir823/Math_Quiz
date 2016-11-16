3cc050e58866a30d2a196ba61eef9d0a35ad936d
filip <filipdautovic@yahoo.com>
2016-11-16 12:11:22 +0100

    gut

--- src/Main_app.java
+++ src/Main_app.java
@@ -5,8 +5,9 @@ public class Main_app {
 		// TODO Auto-generated method stub
 	}
 
-	public static void addition() {
-
+	public static boolean addition() {
+		double num1 = 1 + Math.random() * 10;
+		double num2 = 1 + Math.random() * 10;
 	}
 
 	public static void substraction() {
