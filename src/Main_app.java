80432656ff4347cf89763f45f4d79c22025c8e06
filip <filipdautovic@yahoo.com>
2016-11-16 12:37:29 +0100

    addition ispolirana

--- src/Main_app.java
+++ src/Main_app.java
@@ -4,20 +4,26 @@ public class Main_app {
 
 	public static void main(String[] args) {
 
+	
+		System.out.println(addition());
 		}
 
 	public static boolean addition() {
 		
-		int num1 = (int) Math.random() * 10;
-		int num2 = (int) Math.random() * 10;
+		int num1 = 1 + (int)(Math.random() * 10);
+		int num2 = 1 + (int)(Math.random() * 10);
 		
 		int rez = num1 + num2;
+		
+		System.out.println("Unesite odgovor: " +num1+ " + " +num2);
 		int user = (int)scanner();
 		
 		if(rez == user){
+			System.out.println("Odgovor tacan.");
 			return true;
+			
 		}
-
+		System.out.println("Odgovor netacan.");
 		return false;	
 	
 	}
