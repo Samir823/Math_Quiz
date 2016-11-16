1b6f0b417c49ebed98ffbb87c88695598b8bb04b
Samir Kahvedzic <samirkafa@gmail.com>
2016-11-16 11:50:34 +0100

    Dopuna

--- src/Main_app.java
+++ src/Main_app.java
@@ -1,8 +1,30 @@
-
 public class Main_app {
 
 	public static void main(String[] args) {
+
 		// TODO Auto-generated method stub
+	}
+
+	public static void addition() {
+
+	}
+
+	public static void substraction() {
+
+	}
+
+	public static void multiplication() {
+
+	}
+
+	public static void division() {
+
+	}
+
+	public static void squareRoot(double a) {
+	}
+
+	public static void pow(double a) {
 
 	}
 
